a5e89c48bff7b68109d70e084c3cbd9ccd9b8f70
stloder <scl963@gmail.com>
2018-03-05 12:20:35 -0500

    added cat, head and tail

--- bash.js
+++ bash.js
@@ -3,14 +3,22 @@ let commands = require('./commands');
 let date = commands.date,
     ls = commands.ls,
     prompt = commands.prompt,
-    pwd = commands.pwd;
+    pwd = commands.pwd,
+    echo = commands.echo,
+    cat = commands.cat,
+    head = commands.head,
+    tail = commands.tail;
+    
 
 // Output a prompt
 process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', (data) => {
-  let cmd = data.toString().trim().toLowerCase();
+  let dataArr = data.toString().trim().split(" ");
+
+  let cmd = dataArr[0].toLowerCase();
+  let args = dataArr.slice(1);
 
   if (cmd === 'pwd') {
     pwd();
@@ -18,8 +26,16 @@ process.stdin.on('data', (data) => {
     date();
   } else if (cmd === 'ls') {
     ls();
+  } else if (cmd === 'echo') {
+    echo(args);
+  } else if (cmd === 'cat') {
+    cat(args[0]);
+  } else if (cmd === 'head'){
+    head(args[0]);
+  } else if (cmd === 'tail'){
+    tail(args[0]);
   } else {
-    process.stdout.write('You typed: ' + cmd);
+    process.stdout.write('Command not found: ' + cmd);
     prompt();
   }
 });
--- commands.js
+++ commands.js
@@ -1,12 +1,12 @@
 let fs = require('fs');
 
-function date () {
+function date (file) {
   let currentDate = new Date(Date.now());
   process.stdout.write(currentDate.toString());
   prompt();
 }
 
-function ls () {
+function ls (file) {
   fs.readdir('.', (err, files) => {
     if (err) throw err;
     files.forEach( (file) => {
@@ -16,11 +16,61 @@ function ls () {
   });
 }
 
-function prompt () {
+function cat (file) {
+  var filePath = process.env.PWD + '/' + file;
+  fs.readFile(filePath, 'utf8', callBackOutput);
+  prompt();
+}
+
+function catOutput (err, data) {
+  process.stdout.write(data);
+}
+
+function headOutput (err, data) {
+  data = data.split("\n")
+  data = data.slice(0, 5);
+  data = data.join("\n");
+  process.stdout.write(data);
+}
+
+function head (file) {
+  var filePath = process.env.PWD + '/' + file;
+  fs.readFile(filePath, 'utf8', headOutput);
+  prompt();
+}
+
+function tailOutput (err, data) {
+  data = data.split("\n")
+  data = data.slice(-5);
+  data = data.join("\n");
+  process.stdout.write(data);
+}
+
+function tail (file) {
+  var filePath = process.env.PWD + '/' + file;
+  fs.readFile(filePath, 'utf8', tailOutput);
+  prompt();
+}
+
+function echo (args) {
+  args = args.map((element) => {
+    if(element[0] === '$'){
+      if(process.env.hasOwnProperty(element.slice(1))) {
+        return process.env[element.slice(1)];
+      } 
+      } else {
+        return element;
+    }
+  })
+  process.stdout.write(args.join(" "));
+  prompt();
+}
+
+function prompt (file) {
   process.stdout.write('\nprompt > ');
 }
 
-function pwd () {
+function pwd (file) {
   process.stdout.write(process.env.PWD);
   prompt();
 }
@@ -28,6 +78,10 @@ function pwd () {
 module.exports = {
   date,
   ls,
+  cat,
+  head,
+  tail,
+  echo,
   pwd,
   prompt
 }
